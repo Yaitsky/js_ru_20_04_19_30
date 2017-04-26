dc7825629e188a434b0e5e78a212e25142525f4c
Roman <kaboom.ua@gmail.com>
2017-04-26 17:22:26 +0300

    Update CommentsList.js

--- src/CommentsList.js
+++ src/CommentsList.js
@@ -4,6 +4,7 @@ export default function CommentsList({ comments }) {
     var elements;
     if (comments) {
         elements = comments.map((comment) => {
+        //в идеале Comment тоже в отдельный компонент вынести
         return (
             <li key={comment.id}>
                 <h3>{comment.user}</h3>
