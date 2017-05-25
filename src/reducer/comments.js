67ee6b25e846ba98fb94178e3874d0f0fc4f7654
Roman <kaboom.ua@gmail.com>
2017-05-25 10:46:55 +0300

    Update comments.js

--- src/reducer/comments.js
+++ src/reducer/comments.js
@@ -27,7 +27,9 @@ export default (comments = new DefaultReducerState(), action) => {
             return comments.set('loading', true)
 
         case LOAD_COMMENTS + SUCCESS:
+            //каждый раз перезатираешь комменты, используй merge
             return comments.set('entities', arrayToMap(payload.response, CommentModel))
+                //здесь так просто уже не выйдет, ведь ты загружаешь не все комменты сразу, а отдельно для каждой статьи
                 .set('loading', false)
                 .set('loaded', true)
     }
